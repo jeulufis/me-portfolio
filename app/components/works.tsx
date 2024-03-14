8bd941f893e1ef1905ee52d7c22d4da914a44f26
jeulufis <juan.eulufi.sa@gmail.com>
2024-03-14 15:30:33 -0300

    fixed image

--- app/components/works.tsx
+++ app/components/works.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { dataWorks } from "../data/dataWorks";
 
 export default async function Works() {
@@ -8,7 +9,7 @@ export default async function Works() {
           key={index}
           className="w-full rounded-md bg-gray-50 cursor-pointer transition ease-in-out hover:-translate-y-2 duration-500 hover:shadow-md"
         >
-          <img src="https://png.pngtree.com/background/20230524/original/pngtree-sad-pictures-for-desktop-hd-backgrounds-picture-image_2705986.jpg" className="h-30 w-full" alt={item.title} />
+          <Image src="/loading.jpg" width={500} alt={item.title} />
           <div className="m-3">
             <p className="font-medium">{item.title}</p>
             <span className="text-gray-700 leading-loose font-light">
